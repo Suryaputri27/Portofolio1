1b09352e30f11abdd21eec947140cb2ba6388e94
Surya Putri <makhlukhidup404@gmail.com>
2024-06-26 21:30:16 +0800

    ini commit saya

---
+++ PortoPhut.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet">
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+crossorigin="anonymous"
+referrerpolicy="no-referrer"
+/>
+<title>PortoPut</title>
+</head>
+<body>
+  <!-- header -->
+<header class="sticky">
+  <a href="#" class="logo">Phut</a>
+  <ul class="navlist">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#portofolio">Portofolio</a></li>
+      <li><a href="#contact">Contact</a></li>
+  </ul>
+ 
+  </div>
+</header>
+<!-- home -->
+<section class="home" id="home">
+  <img src="gambar/put.jpg" alt="" >
+  <div class="home-text">
+      <p>Hello! My name is Surya Putri Saini, a student at MAN Ende.
+         I am currently in eleventh grade. I have an interest in various activities at school, especially in the exploration of science and art. I believe that through these various activities,
+         I can continue to learn and develop as a strong and creative individual.</p>
+         <a href="about.php"class="btn">About me </a>
+  </div> 
+          
+</section>
+<!-- feature -->
+<section class="feature"  id="portofolio">
+  <div class="center-text">
+      <h2>Portofolio</h2>
+
+  </div>
+  <div class="feature-content">
+      <div class="row">
+          <img src="gambar/perpustakaan.png" >
+          <div class="arrow">
+              <a href="projek/perpustakkan/index.html"><i class="bx bx-right-arrow-alt"></i></a>
+          </div>
+      </div>
+
+      <div class="row">
+        <img src="gambar/Folio.png" >
+        <div class="arrow">
+            <a href="projek/surya putri html/"><i class="bx bx-right-arrow-alt"></i></a>
+        </div>
+    </div>
+
+    <div class="row">
+        <img src="gambar/Jualan.png" >
+        <div class="arrow">
+            <a href="projek/Surya Putri Sainii/penjualan.html"><i class="bx bx-right-arrow-alt"></i></a>
+        </div>
+    </div>
+
+    <div class="row">
+        <img src="gambar/form.png" >
+        <div class="arrow">
+            <a href="projek/Surya Putri Sainy/form.html"><i class="bx bx-right-arrow-alt"></i></a>
+        </div>
+    </div>
+
+    <div class="row">
+        <img src="gambar/perpustakaan.png" >
+        <div class="arrow">
+            <a href="projek/perpustakkan/index.html"><i class="bx bx-right-arrow-alt"></i></a>
+        </div>
+    </div>
+
+    <div class="row">
+        <img src="gambar/perpustakaan.png" >
+        <div class="arrow">
+            <a href="projek/perpustakkan/index.html"><i class="bx bx-right-arrow-alt"></i></a>
+        </div>
+    </div>
+      </div>
+  </div>
+
+</section>
+
+<!-- new -->
+<section class="new" id="about">
+  <div class="center-text">
+      <h2>Me and my friends</h2>
+
+  </div>
+<div class="new-content">
+  <div class="box">
+      <img src="gambar/rpl.jpg" width="223px" height="300px" alt="">
+      <div class="sale">
+        <p>I am very happy with my classmates. We can always laugh together at recess and support each other in class.</p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/mereka.jpg" width="223px" height="300px" alt="">
+      <div class="sale">
+          <p>What I appreciate most about my friends in the hostel is their diversity.  </p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/berbuka.jpg" width="223px" height="300px"alt="">
+      <div class="sale">
+         <p>We also often hold events together, such as small parties to celebrate birthdays or other special celebrations.</p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/drama.jpg" width="223px" height="300px" alt="">
+      <div class="sale">
+         <p>I am very happy with my classmates. We can always laugh together at recess and support each other in class. </p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/kami.jpg" width="223px" height="300px" alt="">
+      <div class="sale">
+         <p>Every day at the hostel is always full of laughter and stories.</p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/pramuka.jpg" width="223px"height="300px"alt="">
+      <div class="sale">
+         <p>I am very happy with my classmates. We can always laugh together at recess and support each other in class. </p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/perpisahan.jpg" width="223px" height="300px"alt="">
+      <div class="sale">
+          <p>thoughts of separation started to come to me. I don't feel ready.</p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/kelas.jpg" width="223px" height="300px"alt="">
+      <div class="sale">
+         <p>I am very happy with my classmates. We can always laugh together at recess and support each other in class. </p>
+      </div>
+  </div>
+  <div class="box">
+      <img src="gambar/mereka.jpg" width="223px" height="300px"alt="">
+      <div class="sale">
+          <p>thoughts of separation started to come to me. I don't feel ready.</p>
+      </div>
+  </div>
+
+
+</div>
+</section>
+<!--  new-->
+<!-- ===== portofolio contact start===== -->
+<section class="contact section " id="contact">
+<!-- <div class="cont"> -->
+  <div class="container">
+
+      
+    <h3 class="contact-title">get to know more deeply</h3>
+    <h4 class="contact-sub-title"></h4>
+    <!-- contacct form -->
+    
+      <div class="contact-form">
+       <div class="row1">
+          <div class="form-item col-6 padd-15 ">
+            <div class="form-group ">
+              <input
+                type="text"
+                class="form-control shadow-dark"
+                placeholder="Name"
+              />
+            </div>
+          </div>
+          <div class="form-item col-6 padd-15">
+            <div class="form-group">
+              <input
+                type="Email"
+                class="form-control shadow-dark"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+        </div>
+        
+          <div class="form-item col-12 ">
+            <div class="form-group">
+              <input
+                type="Text"
+                class="form-control shadow-dark"
+                placeholder="item"
+              />
+            </div>
+          </div>
+      
+        
+          <div class="form-item col-12 ">
+            <div class="form-group">
+              <textarea
+                name="message"
+                class="form-control shadow-dark"
+                id=""
+                placeholder="message"
+              ></textarea>
+            </div>
+          </div>
+        </div>
+       
+          <div class="form-item col-12 padd-15">
+           <div class="btn">Send</div>
+          
+          </div>
+        
+      </div>
+    </div>
+  </section>
+
+  <section class="footer">
+    <div class="box-container">
+    
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="Product.php"><i class="fas fa-angle-right"></i> Portofolio</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i> contact</a>
+        </div>
+    
+        <div class="box">
+            <h3>Contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 081239068428</a>
+            <a href="#"> <i class="fas fa-envelope"></i> putrisainy@gmail.com</a>
+            <a href="#"> <i class="fas fa-map"></i> Jl. Raya Ende-Bajawa Km 21</a>
+        </div>
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook"></i>Facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+        </div>
+    
+    </div>
+    <div class="credit">creted by <span>miss. putri sainy</span> |Lop yuu</div>
+    </section>
+
+<!-- ===== portofolio contact end===== -->
+
+<!-- custom js link -->
+  <script src="js/script.js"></script>
+</body>
+</html>
---
+++ about.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&display=swap" rel="stylesheet">
+<link
+rel="stylesheet"
+href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+crossorigin="anonymous"
+referrerpolicy="no-referrer"
+/>
+<title>PortoPut</title>
+</head>
+<body>
+  <!-- header -->
+<header class="sticky">
+   
+<section>
+  <a href="#" class="logo">Phut</a>
+  <ul class="navlist">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#feature">About</a></li>
+      <li><a href="#new">Portofolio</a></li>
+      <li><a href="#order">Contact</a></li>
+  </ul>
+</section>
+
+
+
+
+
+  <section class="about-mf sect-pt4 route">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="box-shadow-full">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="row">
+                    <div class="col-sm-6 col-md-5">
+                      <div class="about-img">
+                        <img src="gambar/put.jpg" class="img-fluid rounded b-shadow-a" alt="">
+                      </div>
+                    </div>
+                     <div class="col-sm-6 col-md-7">
+                      <div class="about-info">
+                        <p><span class="title-s">Name: </span> <span>Surya Putri Saini</span></p>
+                        <p><span class="title-s">Profile: </span> <span>Students</span></p>
+                        <p><span class="title-s">Email: </span> <span>Putrisainy629@gmail.com</span></p>
+                        <p><span class="title-s">Phone: </span> <span>081239068428</span></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="skill-mf">
+                    <p class="title-s">Skill</p>
+                    <span>HTML</span> <span class="pull-right">85%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>CSS3</span> <span class="pull-right">75%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>PHP</span> <span class="pull-right">50%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <span>JAVASCRIPT</span> <span class="pull-right">90%</span>
+                    <div class="progress">
+                      <div class="progress-bar" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div> 
+                </div>
+                <div class="col-md-6">
+                  <div class="about-me pt-4 pt-md-0">
+                    <div class="title-box-2">
+                      <h5 class="title-left">
+                        About me
+                      </h5>
+                    </div>
+                    <p class="lead">Hello! I am Surya Putri Saini, 16 years old, a student at MAN Ende. 
+                        I really enjoy meal times and have a desire to gain weight.
+                         I believe that maintaining a healthy diet is the key to achieving my health and fitness goals. Apart from that, 
+                        I am also active in school activities which give me the opportunity to continue to develop and learn. </p>
+                   
+                    <p class="lead">I am Surya Putri Saini, the fourth of six children. I have big dreams to pursue success like my brothers.
+                         They are role models for me and motivate me to continue to strive hard to achieve my goals. 
+                        I believe that with hard work and strong determination, I can make this dream come true.</p>
+                    
+                    <p class="lead">living in a hostel at the moment and feel very lucky to have such understanding friends.
+                         With them, we always support each other and go through various ups and downs together.
+                          Our relationship is very close and full of affection. I really appreciate this togetherness because they have not only become friends, 
+                        but also family for me in a place far from home.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+
+  
+
+ <section class="footer">
+    <div class="box-container">
+    
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"><i class="fas fa-angle-right"></i> about</a>
+            <a href="Product.php"><i class="fas fa-angle-right"></i> Product</a>
+            <a href="contact.php"><i class="fas fa-angle-right"></i> contact</a>
+        </div>
+    
+        <div class="box">
+            <h3>Quick Links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+        </div>
+    
+        <div class="box">
+            <h3>Contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> 081239068428</a>
+            <a href="#"> <i class="fas fa-envelope"></i> putrisainy@gmail.com</a>
+            <a href="#"> <i class="fas fa-map"></i> Jl. Raya Ende-Bajawa Km 21</a>
+        </div>
+        <div class="box">
+            <h3>Follow Us</h3>
+            <a href="#"><i class="fab fa-facebook"></i>Facebook</a>
+            <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+        </div>
+    
+    </div>
+    <div class="credit">creted by <span>miss. putri sainy</span> |Lop yuu</div>
+    </section> 
+
+<!-- ===== portofolio contact end===== -->
+
+<!-- custom js link -->
+  <script src="js/script.js"></script>
+</body>
+</html>
